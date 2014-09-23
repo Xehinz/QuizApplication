a9c469c2cdab944051a486a9f1fab70f9e315f64
Xehinz <tim.cool@gmail.com>
2014-09-23 16:20:22 +0200

    Added package & Main class
    Pacakage quizApplication added
    Main class added

---
+++ src/quizApplication/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package quizApplication;
+
+/**
+ * @author Cool Tim
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
