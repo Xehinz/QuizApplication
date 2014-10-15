c4f56c093d68175ae8476a0f965d356e2d98eb19
BenVandenberk <Ben.Vandenberk@hotmail.com>
2014-10-15 18:19:42 +0200

    DatumTesting toegevoegd
    Klasse DatumTest toegevoegd
    minimum packages gedefinieerd

---
+++ src/util/datumWrapper/Datum.java
@@ -0,0 +1,280 @@
+package util.datumWrapper;
+
+import static java.lang.Math.abs;
+
+import java.text.DateFormatSymbols;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * Custom Datum klasse die de java.util.GregorianCalendar klasse wrapt
+ *
+ * @author Bert Neyt
+ * @author Adriaan Kuipers
+ * @author Ben Vandenberk
+ * @version 4/10/2014
+ *
+ */
+public class Datum implements Comparable<Datum> {
+
+	private GregorianCalendar gregorianCalendarDatum;
+
+	/**
+	 * Maakt een datum object aan met als waarde de dag van vandaag
+	 */
+	public Datum() {
+		GregorianCalendar vandaag = new GregorianCalendar();
+		setDatum(vandaag.get(Calendar.DAY_OF_MONTH), vandaag.get(Calendar.MONTH) + 1, vandaag.get(Calendar.YEAR));
+	}
+
+	/**
+	 * Maakt een datum object aan
+	 *
+	 * @param dag
+	 *            de dag van de maand
+	 * @param maand
+	 *            de maand
+	 * @param jaar
+	 *            het jaar
+	 *
+	 * @throws IllegalArgumentException
+	 *             als een van de argumenten of de combinatie van argumenten ongeldig is
+	 */
+	public Datum(int dag, int maand, int jaar) {
+		boolean b = this.setDatum(dag, maand, jaar);
+		if (!b) {
+			throw new IllegalArgumentException("Ongeldige datum!");
+		}
+	}
+
+	/**
+	 * Maakt een datum object aan op basis van een ander datum object
+	 *
+	 * @param datum
+	 *            het datum object op basis waarvan de constructor een nieuw datum object aanmaakt
+	 */
+	public Datum(Datum datum) {
+		this(datum.getDag(), datum.getMaand(), datum.getJaar());
+	}
+
+	/**
+	 * Maakt een datum object aan op basis van een string. Format: DD/MM/YYYY
+	 *
+	 * @param datum
+	 *            de String met een datum waarde (DD/MM/YYYY)
+	 * @throws IllegalArgumentException
+	 *             als de input String van een foutief formaat is of als de datumwaarde ongeldig is
+	 */
+	public Datum(String datum) {
+		String[] parts = datum.split("/");
+		String dag = parts[0];
+		String maand = parts[1];
+		String jaar = parts[2];
+		try {
+			boolean b = this.setDatum(Integer.parseInt(dag), Integer.parseInt(maand), Integer.parseInt(jaar));
+			if (!b) {
+				throw new IllegalArgumentException("Ongeldige datum");
+			}
+		} catch (NumberFormatException N) {
+			throw new IllegalArgumentException("Foutieve input", N);
+		}
+	}
+
+	/**
+	 * Haalt het jaar-gedeelte van het datum object op
+	 *
+	 * @return het jaar
+	 */
+	public int getJaar() {
+		return this.gregorianCalendarDatum.get(GregorianCalendar.YEAR);
+	}
+
+	/**
+	 * Haalt het maand-gedeelte van het datum object op
+	 *
+	 * @return de maand
+	 */
+	public int getMaand() {
+		return this.gregorianCalendarDatum.get(GregorianCalendar.MONTH) + 1;
+	}
+
+	/**
+	 * Haalt het dag-gedeelte van het datum object op
+	 *
+	 * @return de dag
+	 */
+	public int getDag() {
+		return this.gregorianCalendarDatum.get(GregorianCalendar.DAY_OF_MONTH);
+	}
+
+	/**
+	 * Zet het datum object op een geldige waarde
+	 *
+	 * @param dag
+	 *            de dag van de maand
+	 * @param maand
+	 *            de maand
+	 * @param jaar
+	 *            het jaar
+	 * @return true als het zetten van de datum gelukt is
+	 */
+	public boolean setDatum(int dag, int maand, int jaar) {
+		try {
+			if (jaar < 0 || jaar > 9999) {
+				return false;
+			}
+			this.gregorianCalendarDatum = new GregorianCalendar(jaar, maand - 1, dag);
+			this.gregorianCalendarDatum.setLenient(false);
+			this.gregorianCalendarDatum.getTime();
+
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	/**
+	 * Geeft een string terug in Amerikaans formaat (YYYY/MM/DD)
+	 *
+	 * @return een string in Amerikaans formaat (YYYY/MM/DD)
+	 */
+	public String getDatumInAmerikaansFormaat() {
+		return this.getJaar() + "/" + this.getMaand() + "/" + this.getDag();
+	}
+
+	/**
+	 * Geeft een string terug in Europees formaat (DD/MM/YYYY)
+	 *
+	 * @return een string in Europees formaat (DD/MM/YYYY)
+	 */
+	public String getDatumInEuropeesFormaat() {
+		return this.getDag() + "/" + this.getMaand() + "/" + this.getJaar();
+	}
+
+	@Override
+	public String toString() {
+		return this.getDag() + " " + new DateFormatSymbols().getMonths()[this.getMaand() - 1] + " " + this.getJaar();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Datum) {
+			return this.getDatumInAmerikaansFormaat().equals(((Datum) obj).getDatumInAmerikaansFormaat());
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public int compareTo(Datum datum) {
+		GregorianCalendar inputDatum = new GregorianCalendar(datum.getJaar(), datum.getMaand() - 1, datum.getDag());
+		return this.gregorianCalendarDatum.compareTo(inputDatum);
+	}
+
+	/**
+	 * Geeft true terug als het datum object zich chronologisch vóór het meegegeven datum object bevindt
+	 *
+	 * @param datum
+	 *            het datum object waarmee vergeleken wordt
+	 * @return true als het datum object zich chronologisch vóór het meegegeven datum object bevindt
+	 */
+	public boolean kleinerDan(Datum datum) {
+		GregorianCalendar inputDatum = new GregorianCalendar(datum.getJaar(), datum.getMaand() - 1, datum.getDag());
+		return inputDatum.before(this.gregorianCalendarDatum);
+	}
+
+	/**
+	 * Geeft het aantal gehele jaren terug dat zich bevindt tussen het datum object en het meegegeven datum object
+	 *
+	 * @param datum
+	 *            het datum object waarmee vergeleken wordt
+	 * @return het aantal gehele jaren dat zich bevindt tussen het datum object en het meegegeven datum object
+	 */
+	public int verschilInJaren(Datum datum) {
+		GregorianCalendar inputDatum = new GregorianCalendar(datum.getJaar(), datum.getMaand() - 1, datum.getDag());
+		if (this.equals(datum)) {
+			return 0;
+		}
+		int verschil = -1;
+		if (this.gregorianCalendarDatum.after(inputDatum)) {
+			while (inputDatum.before(this.gregorianCalendarDatum)) {
+				inputDatum.add(Calendar.YEAR, 1);
+				verschil++;
+			}
+		} else {
+			GregorianCalendar thisClone = new GregorianCalendar(this.getJaar(), this.getMaand() - 1, this.getDag());
+			while (thisClone.before(inputDatum)) {
+				thisClone.add(Calendar.YEAR, 1);
+				verschil++;
+			}
+		}
+		return verschil;
+	}
+
+	/**
+	 * Geeft het aantal gehele maanden terug dat zich bevindt tussen het datum object en het meegegeven datum object
+	 *
+	 * @param datum
+	 *            het datum object waarmee vergeleken wordt
+	 * @return het aantal gehele maanden dat zich bevindt tussen het datum object en het meegegeven datum object
+	 */
+	public int verschilInMaanden(Datum datum) {
+		GregorianCalendar inputDatum = new GregorianCalendar(datum.getJaar(), datum.getMaand() - 1, datum.getDag());
+		if (this.equals(datum)) {
+			return 0;
+		}
+		int verschil = -1;
+		if (this.gregorianCalendarDatum.after(inputDatum)) {
+			while (inputDatum.before(this.gregorianCalendarDatum)) {
+				inputDatum.add(Calendar.MONTH, 1);
+				verschil++;
+			}
+		} else {
+			GregorianCalendar thisClone = new GregorianCalendar(this.getJaar(), this.getMaand() - 1, this.getDag());
+			while (thisClone.before(inputDatum)) {
+				thisClone.add(Calendar.MONTH, 1);
+				verschil++;
+			}
+		}
+		return verschil;
+	}
+
+	/**
+	 * Geeft het aantal gehele dagen terug dat zich bevindt tussen het datum object en het meegegeven datum object
+	 *
+	 * @param datum
+	 *            het datum object waarmee vergeleken wordt
+	 * @return het aantal gehele dagen dat zich bevindt tussen het datum object en het meegegeven datum object
+	 */
+	public int verschilInDagen(Datum datum) {
+		GregorianCalendar inputDatum = new GregorianCalendar(datum.getJaar(), datum.getMaand() - 1, datum.getDag());
+		long verschil = abs(inputDatum.getTimeInMillis() - this.gregorianCalendarDatum.getTimeInMillis());
+		return (int) (verschil / (1000 * 60 * 60 * 24));
+	}
+
+	/**
+	 * Verandert de datum met het meegegeven aantal dagen. Een positief getal verhoogt de datum, een negatief getal
+	 * verlaagt ze
+	 *
+	 * @param aantalDagen
+	 *            het aantal dagen waarmee de datum aangepast wordt
+	 */
+	public void veranderDatum(int aantalDagen) {
+		this.gregorianCalendarDatum.add(Calendar.DAY_OF_YEAR, aantalDagen);
+	}
+
+	/**
+	 * Geeft een datum object terug dat verschilt van het huidige met het meegegeven aantal dagen. Een positieve waarde
+	 * geeft een latere datum terug, een negatieve een eerdere
+	 *
+	 * @param aantalDagen
+	 *            het aantal dagen waarmee de teruggegeven datum verschilt van het datum object
+	 * @return het datum object dat met het aantal dagen is veranderd
+	 */
+	public Datum getVeranderdeDatum(int aantalDagen) {
+		Datum resultaatDatum = new Datum(this);
+		resultaatDatum.veranderDatum(aantalDagen);
+		return resultaatDatum;
+	}
+}
---
+++ testing/util/DatumTest.java
@@ -0,0 +1,246 @@
+package util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import util.datumWrapper.Datum;
+
+public class DatumTest {
+
+	private Datum geldigeDatum, eerdereDatum, latereDatum, zelfdeDatum, datum, datumString, veranderdeDatum;
+
+	@Before
+	public void setUp() {
+		datum = new Datum(11, 10, 2014);
+		datumString = new Datum("30/09/2014");
+		veranderdeDatum = new Datum(14, 10, 2014);
+		geldigeDatum = new Datum(1, 2, 1980);
+		eerdereDatum = new Datum(31, 1, 1980);
+		latereDatum = new Datum(2, 2, 1980);
+		zelfdeDatum = new Datum(1, 2, 1980);
+	}
+
+	@Test
+	public void test_Datum_Datum_datums_gelijk() {
+		Datum datum = new Datum(geldigeDatum);
+		assertEquals("Dag gelijk", geldigeDatum.getDag(), datum.getDag());
+		assertEquals("Maand gelijk", geldigeDatum.getMaand(), datum.getMaand());
+		assertEquals("Jaar gelijk", geldigeDatum.getJaar(), datum.getJaar());
+	}
+
+	@Test
+	public void test_Datum_NoParam_datum_gelijk_aan_systeemdatum() {
+		Datum datum = new Datum();
+		GregorianCalendar gregorianCalendar = new GregorianCalendar();
+		assertEquals("Dag gelijk", gregorianCalendar.get(Calendar.DAY_OF_MONTH), datum.getDag());
+		assertEquals("Maand gelijk", gregorianCalendar.get(Calendar.MONTH) + 1, datum.getMaand());
+		assertEquals("Jaar gelijk", gregorianCalendar.get(Calendar.YEAR), datum.getJaar());
+	}
+
+	@Test
+	public void test_getDatumInAmerikaansFormaat_string_in_juist_formaat() {
+		assertEquals("Datumstrings gelijk", "1980/2/1", geldigeDatum.getDatumInAmerikaansFormaat());
+	}
+
+	@Test
+	public void test_compareTo_zelfde_datum_geeft_0() {
+		assertEquals("Zelfde datum", 0, geldigeDatum.compareTo(zelfdeDatum));
+	}
+
+	@Test
+	public void test_compareTo_eerdere_datum_geeft_positief() {
+		assertTrue(geldigeDatum.compareTo(eerdereDatum) > 0);
+	}
+
+	@Test
+	public void test_compareTo_latere_datum_geeft_negatief() {
+		assertTrue(geldigeDatum.compareTo(latereDatum) < 0);
+	}
+
+	@Test
+	public void test_verschilInDagen_geen_verschil_geeft_0() {
+		assertEquals("Zelfde datum", 0, geldigeDatum.verschilInDagen(zelfdeDatum));
+	}
+
+	@Test
+	public void test_verschilInDagen_groot_verschil_correcte_waarde() {
+		// Juiste verschilwaarde berekend met on-line tool (http://www.timeanddate.com/date/duration.html)
+		Datum datumLangGeleden = new Datum(13, 2, 1599);
+		assertEquals("Verschil in dagen", 139145, geldigeDatum.verschilInDagen(datumLangGeleden));
+	}
+
+	@Test
+	public void test_verschilInDagen_verschillende_datum_minder_dan_24_uur_geeft_1() throws InterruptedException {
+		Datum vandaag = new Datum();
+		Thread.sleep(10);
+		Datum vandaagEenBeetjeLater = new Datum();
+		vandaag.veranderDatum(1);
+		assertEquals("Verschillende dag, minder dan 24 uur verschil", 1, vandaagEenBeetjeLater.verschilInDagen(vandaag));
+	}
+
+	@Test
+	public void test_Datum_3int_gelijk_aan_input() {
+		assertEquals("Dag gelijk", 1, geldigeDatum.getDag());
+		assertEquals("Maand gelijk", 2, geldigeDatum.getMaand());
+		assertEquals("Jaar gelijk", 1980, geldigeDatum.getJaar());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_Datum_3int_ongeldige_dag() {
+		Datum datum = new Datum(31, 4, 2000);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_Datum_3int_ongeldige_maand() {
+		Datum datum = new Datum(4, 15, 2000);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_Datum_3int_ongeldig_jaar() {
+		Datum datum = new Datum(4, 4, 50000);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_Datum_3int_ongeldige_schrikkeldag() {
+		Datum datum = new Datum(29, 2, 1999);
+	}
+
+	@Test
+	public void test_setDatum_gelijk_aan_input() {
+		geldigeDatum.setDatum(4, 4, 2000);
+		assertEquals("Dag gelijk", 4, geldigeDatum.getDag());
+		assertEquals("Maand gelijk", 4, geldigeDatum.getMaand());
+		assertEquals("Jaar gelijk", 2000, geldigeDatum.getJaar());
+	}
+
+	@Test
+	public void test_setDatum_ongeldige_dag_geeft_false() {
+		assertFalse(geldigeDatum.setDatum(31, 4, 2000));
+	}
+
+	@Test
+	public void test_setDatum_ongeldige_maand_geeft_false() {
+		assertFalse(geldigeDatum.setDatum(4, 15, 2000));
+	}
+
+	@Test
+	public void test_setDatum_ongeldig_jaar_geeft_false() {
+		assertFalse(geldigeDatum.setDatum(4, 4, 50000));
+	}
+
+	@Test
+	public void test_setDatum_ongeldige_schrikkeldag_geeft_false() {
+		assertFalse(geldigeDatum.setDatum(29, 2, 1999));
+	}
+
+	@Test
+	public void test_equals_zelfde_datum_geeft_true() {
+		assertTrue(geldigeDatum.equals(zelfdeDatum));
+	}
+
+	@Test
+	public void test_equals_null_geeft_false() {
+		assertTrue(!geldigeDatum.equals(null));
+	}
+
+	@Test
+	public void test_equals_andere_klasse_geeft_false() {
+		Object obj = new Object();
+		assertTrue(!geldigeDatum.equals(obj));
+	}
+
+	@Test
+	public void test_equals_andere_dag_geeft_false() {
+		Datum datum = new Datum(2, 2, 1980);
+		assertTrue(!geldigeDatum.equals(datum));
+	}
+
+	@Test
+	public void test_equals_andere_maand_geeft_false() {
+		Datum datum = new Datum(1, 4, 1980);
+		assertTrue(!geldigeDatum.equals(datum));
+	}
+
+	@Test
+	public void test_equals_ander_jaar_geeft_false() {
+		Datum datum = new Datum(1, 2, 1984);
+		assertTrue(!geldigeDatum.equals(datum));
+	}
+
+	@Test
+	public void test_verschilInMaanden_correcte_waarde() {
+		Datum datum = new Datum(5, 1, 1981);
+		assertEquals("Verschil in Maanden", 11, geldigeDatum.verschilInMaanden(datum));
+	}
+
+	@Test
+	public void test_verschilInMaanden_geen_verschil_geeft_0() {
+		assertEquals("Zelfde datum", 0, geldigeDatum.verschilInDagen(zelfdeDatum));
+	}
+
+	@Test
+	public void test_verschilInMaanden_kleiner_dan_1_geeft_0() {
+		Datum datum = new Datum(5, 1, 1980);
+		assertEquals("Verschil in Maanden", 0, geldigeDatum.verschilInMaanden(datum));
+	}
+
+	@Test
+	public void test_Datum_Met_String_Datum_wordt_aangemaakt() {
+		assertEquals(30, datumString.getDag());
+		assertEquals(9, datumString.getMaand());
+		assertEquals(2014, datumString.getJaar());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_Datum_Met_String_faalt_voor_ongeldige_string() {
+		datum = new Datum("3a/09/2014");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_Datum_Met_String_faalt_voor_ongeldig_formaat() {
+		datum = new Datum("01/28/2014");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_Datum_Met_String_faalt_voor_ongeldige_dag() {
+		datum = new Datum("29/02/2014");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_Datum_Met_String_faalt_voor_ongeldige_maand() {
+		datum = new Datum("29/24/2014");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void test_Datum_Met_String_faalt_voor_ongeldig_jaar() {
+		datum = new Datum("29/02/10200");
+	}
+
+	@Test
+	public void test_getDatumInEuropeesFormaat_Geeft_Juiste_Formaat() {
+		assertEquals("11/10/2014", datum.getDatumInEuropeesFormaat());
+	}
+
+	@Test
+	public void test_kleinerDan_true_voor_parameter_Datum_die_kleiner_is() {
+		assertTrue(datum.kleinerDan(datumString));
+	}
+
+	@Test
+	public void test_kleinerDan_false_voor_parameter_Datum_die_groter_is() {
+		assertFalse(datumString.kleinerDan(datum));
+	}
+
+	@Test
+	public void test_veranderDatum_geeft_juiste_datum() {
+		datum.veranderDatum(3);
+		assertEquals(datum, veranderdeDatum);
+	}
+}
