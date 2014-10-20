c1f9962da5ecb0c0417dd3b351c8bee2b69b423c
bertneyt <neytbert@gmail.com>
2014-10-20 20:42:15 +0200

    OpdrachtCatalogus en QuizCatalogus

--- src/model/OpdrachtCatalogus.java
+++ src/model/OpdrachtCatalogus.java
@@ -1,5 +1,28 @@
 package model;
 
-public class OpdrachtCatalogus {
+import java.util.List;
 
+public class OpdrachtCatalogus {
+private List<Opdracht> opdrachtcatalogus;
+	
+	public OpdrachtCatalogus(List<Opdracht> oc){
+		this.opdrachtcatalogus = oc;
+	}
+	
+	public void addOpdracht(Opdracht O){
+		this.opdrachtcatalogus.add(O);
+	}
+	
+	public void removeOpdracht(Opdracht O){
+		this.opdrachtcatalogus.remove(O);
+	}
+	
+	public Opdracht getOpdracht(int volgnr){
+		return this.opdrachtcatalogus.get(volgnr-1);
+	}
+	
+	public int count(){
+		return this.opdrachtcatalogus.size();
+	}
+	
 }
--- src/model/QuizCatalogus.java
+++ src/model/QuizCatalogus.java
@@ -1,5 +1,27 @@
 package model;
 
-public class QuizCatalogus {
+import java.util.List;
 
+public class QuizCatalogus {
+	private List<Quiz> quizcatalogus;
+
+	public QuizCatalogus(List<Quiz> qc) {
+		this.quizcatalogus = qc;
+	}
+
+	public void addOpdracht(Quiz Q) {
+		this.quizcatalogus.add(Q);
+	}
+
+	public void removeOpdracht(Quiz Q) {
+		this.quizcatalogus.remove(Q);
+	}
+	
+	public Quiz getQuiz(int volgnr){
+		return this.quizcatalogus.get(volgnr-1);
+	}
+	
+	public int count(){
+		return this.quizcatalogus.size();
+	}
 }
