ad27d59c695af402a291a6948a1a6a934a3e24bd
jefke222 <jefke222@hotmail.com>
2014-10-23 18:13:51 +0200

    Methodes LeerlingContainer
    getLeerling, ArrayList, addLeerling, removeLeerling toegevoegd

--- src/model/LeerlingContainer.java
+++ src/model/LeerlingContainer.java
@@ -1,5 +1,34 @@
 package model;
 
+import java.util.ArrayList;
+
 public class LeerlingContainer {
 
+	private Leerling leerling;
+	
+	public <leerling> Leerling getLeerling() {
+		
+		return leerling;
+		
+	}
+	
+	private ArrayList<Leerling> Leerlingen;
+	
+	public LeerlingContainer() {
+		Leerlingen = new ArrayList<Leerling>();
+	}
+
+	public void addLeerling(Leerling l) {
+		if (!Leerlingen.contains(l)) {
+			Leerlingen.add(l);
+		}
+		return;
+	}
+
+	public void removeLeerling(Leerling l) {
+		if (Leerlingen.contains(l)) {
+			Leerlingen.remove(l);
+		}
+	}
+	
 }
