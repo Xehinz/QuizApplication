7dd966f7c8dbf04bdda1611b5cc07a8f5ed27631
bertneyt <neytbert@gmail.com>
2014-11-02 20:29:43 +0100

    begin van Opsomming klasse

--- src/model/Opsomming.java
+++ src/model/Opsomming.java
@@ -1,36 +1,54 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Opsomming extends Opdracht implements Valideerbaar {
+
+	private ArrayList<String> opsommingJuisteAntwoord;
+	private boolean inJuisteVolgorde;
 
 	public Opsomming(OpdrachtCategorie opdrachtCategorie, Leraar auteur) {
 		super(opdrachtCategorie, auteur);
 	}
 
-	public Opsomming(String vraag, String juisteAntwoord, OpdrachtCategorie opdrachtCategorie, Leraar auteur) {
+	public Opsomming(String vraag, ArrayList<String> juisteAntwoord,
+			OpdrachtCategorie opdrachtCategorie, Leraar auteur,
+			boolean injuistevolgorde) {
 		super(vraag, opdrachtCategorie, auteur);
-		// Juiste antwoord nog toewijzen
+		this.opsommingJuisteAntwoord = juisteAntwoord;
+		this.inJuisteVolgorde = injuistevolgorde;
 	}
 
-	public Opsomming(String vraag, int maxAantalPogingen, String juisteAntwoord, OpdrachtCategorie opdrachtCategorie,
-			Leraar auteur) {
+	public Opsomming(String vraag, int maxAantalPogingen,
+			ArrayList<String> juisteAntwoord,
+			OpdrachtCategorie opdrachtCategorie, Leraar auteur,
+			boolean injuistevolgorde) {
 		super(vraag, maxAantalPogingen, 0, opdrachtCategorie, auteur);
-		// Juiste antwoord nog toewijzen
+		this.opsommingJuisteAntwoord = juisteAntwoord;
+		this.inJuisteVolgorde = injuistevolgorde;
 	}
 
-	public Opsomming(String vraag, String juisteAntwoord, int maxAntwoordTijd, OpdrachtCategorie opdrachtCategorie, Leraar auteur) {
+	public Opsomming(String vraag, ArrayList<String> juisteAntwoord,
+			int maxAntwoordTijd, OpdrachtCategorie opdrachtCategorie,
+			Leraar auteur, boolean injuistevolgorde) {
 		super(vraag, maxAntwoordTijd, opdrachtCategorie, auteur);
-		// Juiste antwoord nog toewijzen
+		this.opsommingJuisteAntwoord = juisteAntwoord;
+		this.inJuisteVolgorde = injuistevolgorde;
 	}
 
-	public Opsomming(String vraag, String juisteAntwoord, int maxAantalPogingen, int maxAntwoordTijd,
-			OpdrachtCategorie opdrachtCategorie, Leraar auteur) {
-		super(vraag, maxAantalPogingen, maxAntwoordTijd, opdrachtCategorie, auteur);
-		// Juiste antwoord nog toewijzen
+	public Opsomming(String vraag, ArrayList<String> juisteAntwoord,
+			int maxAantalPogingen, int maxAntwoordTijd,
+			OpdrachtCategorie opdrachtCategorie, Leraar auteur,
+			boolean injuistevolgorde) {
+		super(vraag, maxAantalPogingen, maxAntwoordTijd, opdrachtCategorie,
+				auteur);
+		this.opsommingJuisteAntwoord = juisteAntwoord;
+		this.inJuisteVolgorde = injuistevolgorde;
 	}
 
 	@Override
 	public boolean isValide(String antwoord) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -42,8 +60,20 @@ public class Opsomming extends Opdracht implements Valideerbaar {
 
 	@Override
 	public boolean isJuisteAntwoord(String antwoord) {
-		// TODO Auto-generated method stub
-		return false;
+		ArrayList<String> antwoorden = new ArrayList<String>(
+				Arrays.asList(antwoord.split(",")));
+		if (this.inJuisteVolgorde) {
+			if (this.opsommingJuisteAntwoord.equals(antwoorden))
+				return true;
+			else
+				return false;
+		} else {
+			if (this.opsommingJuisteAntwoord.containsAll(antwoorden)
+					&& antwoorden.containsAll(this.opsommingJuisteAntwoord))
+				return true;
+			else
+				return false;
+		}
 	}
 
 	@Override
