f60365c85a8892ee691051c38d67d225f2c2955f
AdriaanKuipers <adriaan.kuipers@gmail.com>
2014-11-21 11:49:38 +0100

    LeerlingContainerTest 100%

--- testing/model/LeerlingContainerTest.java
+++ testing/model/LeerlingContainerTest.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Tests voor LeerlingContainer
@@ -18,7 +19,7 @@ import java.util.ArrayList;
 
 public class LeerlingContainerTest {
 	
-	private LeerlingContainer leerlingContainer;
+	private LeerlingContainer leerlingContainer, copyLeerlingContainer, andereLeerlingContainer;
 	private Leerling leerlingA, leerlingACopy, leerlingB, leerlingC;
 	private ArrayList<Leerling> leerlingen;
 
@@ -32,6 +33,9 @@ public class LeerlingContainerTest {
 		leerlingen.add(leerlingB);
 		leerlingC = new Leerling("Koning", "Filip", 6);
 		leerlingContainer = new LeerlingContainer(leerlingen);
+		copyLeerlingContainer = new LeerlingContainer(leerlingen);
+		leerlingen.add(leerlingC);
+		andereLeerlingContainer = new LeerlingContainer(leerlingen);
 	}
 	
 	@Test
@@ -69,6 +73,7 @@ public class LeerlingContainerTest {
 	
 	@Test
 	public void test_getLeerlingen_OK() {
+		leerlingen.remove(leerlingC);
 		assertEquals("getLeerlingen gelijk aan input", leerlingContainer.getLeerlingen(), leerlingen);
 	}
 	
@@ -87,6 +92,75 @@ public class LeerlingContainerTest {
 		leerlingContainer.getLeerling(5);
 	}
 	
+	@Test
+	public void test_getAlleQuizDeelnames_OK() {
+		ArrayList<QuizDeelname> alleQuizDeelnames = new ArrayList<QuizDeelname>();
+		for (Leerling leerling : leerlingen) {
+			alleQuizDeelnames.addAll(leerling.getQuizDeelnames());
+		}
+		Collections.sort(alleQuizDeelnames);
+		assertEquals("quizdeelnames OK", alleQuizDeelnames, andereLeerlingContainer.getAlleQuizDeelnames());
+	}
 	
+	@Test
+	public void test_getAlleOpdrachtAntwoorden_OK() {
+		ArrayList<OpdrachtAntwoord> alleOpdrachtAntwoorden = new ArrayList<OpdrachtAntwoord>();
+		for (Leerling leerling : leerlingen) {
+			alleOpdrachtAntwoorden.addAll(leerling.getAlleOpdrachtAntwoorden());
+		}
+		Collections.sort(alleOpdrachtAntwoorden);
+		assertEquals("opdrachtantwoorden OK", alleOpdrachtAntwoorden, andereLeerlingContainer.getAlleOpdrachtAntwoorden());
+	}
+	
+	@Test
+	public void test_equals_gelijk_geeft_true() {
+		assertTrue(leerlingContainer.equals(copyLeerlingContainer));
+	}
+	
+	@Test
+	public void test_equals_niet_gelijk_geeft_false() {
+		
+		assertFalse(leerlingContainer.equals(andereLeerlingContainer));
+	}
+	
+	@Test
+	public void test_equals_ander_type_geeft_false() {		
+		assertFalse(leerlingContainer.equals(leerlingC));
+	}
 
+	@Test
+	public void test_clone_OK() {
+		andereLeerlingContainer = leerlingContainer.clone();
+		assertEquals("container na clone is gelijk", leerlingContainer, andereLeerlingContainer);
+	}
+	
+	@Test
+	public void test_compareTo_gelijk_geeft_0() {
+		assertEquals("compareTo geeft 0 bij equal", leerlingContainer.compareTo(copyLeerlingContainer), 0);
+	}
+	
+	@Test
+	public void test_compareTo_groter_geeft_1() {
+		leerlingen.remove(leerlingA);
+		leerlingen.remove(leerlingB);
+		andereLeerlingContainer = new LeerlingContainer(leerlingen);
+		assertEquals("compareTo geeft 0 bij equal", leerlingContainer.compareTo(andereLeerlingContainer), 1);
+	}
+	
+	@Test
+	public void test_compareTo_kleiner_geeft_min1() {
+		assertEquals("compareTo geeft 0 bij equal", leerlingContainer.compareTo(andereLeerlingContainer), -1);
+	}
+	
+	@Test
+	public void test_toString() {
+		String result = String.format(
+				"LeerlingContainer met %d leerlingen:\n\n", andereLeerlingContainer.count());
+		Collections.sort(leerlingen);
+		for (Leerling leerling : leerlingen) {
+			result += leerling + "\n----------------------------------------------------------------------------------------------------\n";
+		}		
+		assertEquals("toString geeft correcte Stringvoorstelling terug", result, andereLeerlingContainer.toString());
+	}
+	
 }
