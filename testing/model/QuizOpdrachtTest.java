fd15cc04e3f566b4e07e173a73d4dea6ccdc6789
jefke222 <jefke222@hotmail.com>
2014-11-23 19:47:28 +0100

    QuizOpdrachtTest 90%
    Test KoppelmOpdrachtAanQuiz, OntkoppelOpdrachtVanQuiz,
    VoegOpdrachtAntwoordToe, VerkrijgGemiddeldeScore, Equals(Nog Afwerken) ,
    CompareTo en ToString afgewerkt

--- testing/model/QuizOpdrachtTest.java
+++ testing/model/QuizOpdrachtTest.java
@@ -1,17 +1,16 @@
 package model;
 
 import static org.junit.Assert.*;
-
 import java.util.ArrayList;
-
+import model.quizStatus.Opengesteld;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Testklasse van de Klasse QuizOpdracht
  *
- * @author Jef Bellens
- * @version 15/11/2014
+ * @author Jef Bellens & Tim Cool
+ * @version 23/11/2014
  *
  */
 
@@ -26,11 +25,9 @@ public class QuizOpdrachtTest {
 	ArrayList<Quiz> quizen;
 	QuizOpdracht quizOpdracht;
 	int maxScore;
-	ArrayList<OpdrachtAntwoord> opdrachtAntwoord = quizOpdracht.getOpdrachtAntwoorden();
 	
 	@Before
 	public void setUp() throws Exception {
-		//maxScore = quizOpdracht.getMaxScore();
 		
 		quiz1 = new Quiz(Leraar.CHARLOTTE_NEVEN, "1ste quiz");
 		quiz2 = new Quiz(Leraar.CHARLOTTE_NEVEN, "2de quiz");
@@ -48,6 +45,8 @@ public class QuizOpdrachtTest {
 		opdracht2 = new KlassiekeOpdracht(OpdrachtCategorie.WETENSCHAPPEN, Leraar.MARIA_AERTS);
 		opdracht3 = new KlassiekeOpdracht(OpdrachtCategorie.NEDERLANDS, Leraar.STEVEN_OPDEBEEK);
 
+		quizOpdracht = QuizOpdracht.koppelOpdrachtAanQuiz(quiz1, opdracht1, 0);
+				
 		opdrachten = new ArrayList<Opdracht>();
 
 		opdrachten.add(opdracht1);
@@ -67,36 +66,79 @@ public class QuizOpdrachtTest {
 	
 	@Test
 	public void testAttachOpdrachtToQuiz_KoppelOpdrachtAanQuiz_CorrecteKoppeling() {
-		QuizOpdracht.koppelOpdrachtAanQuiz(quiz1, opdracht1, 5);
+		QuizOpdracht quizOpdracht = QuizOpdracht.koppelOpdrachtAanQuiz(quiz1, opdracht1, 5);
 		//assertTrue("Quiz1 heeft juiste opdracht", quiz1.getOpdrachten().contains(quizOpdracht));
-		assertTrue("Opdracht1 heeft juiste quiz", opdracht1.getQuizOpdrachten().contains(quiz1));
-		QuizOpdracht.koppelOpdrachtAanQuiz(quiz2, opdracht2, 10);
+		assertTrue("Opdracht1 heeft juiste quiz", opdracht1.getQuizOpdrachten().contains(quizOpdracht));
+		quizOpdracht = QuizOpdracht.koppelOpdrachtAanQuiz(quiz2, opdracht2, 10);
 		assertTrue("Quiz2 heeft juiste opdracht", quiz2.getOpdrachten().contains(opdracht2));
-		assertTrue("Opdracht2 heeft juiste quiz", opdracht2.getQuizOpdrachten().contains(quiz2));
+		assertTrue("Opdracht2 heeft juiste quiz", opdracht2.getQuizOpdrachten().contains(quizOpdracht));
 	}
 	
 	@Test
 	public void testDetachOpdrachtFromQuiz_OntkoppelOpdrachtVanQuiz_CorrecteOntkoppeling() {
-		quiz1.removeQuizOpdracht(quizOpdracht);
-		opdracht1.removeQuizOpdracht(quizOpdracht);
-		assertFalse("Correcte ontkoppeling", (quizOpdracht.getOpdracht().equals(opdracht1)));
-		assertFalse("Correcte ontkoppeling", (quizOpdracht.getQuiz().equals(quiz1)));
+		QuizOpdracht qOpdracht = QuizOpdracht.koppelOpdrachtAanQuiz(quiz1, opdracht1, 5);
+		quiz1.removeQuizOpdracht(qOpdracht);
+		opdracht1.removeQuizOpdracht(qOpdracht);
+		assertFalse("Correcte ontkoppeling", quiz1.getQuizOpdrachten().contains(qOpdracht));
+		assertFalse("Correcte ontkoppeling", opdracht1.getQuizOpdrachten().contains(qOpdracht));
 	}
 	
 	@Test
-	public void AddOpdrachtAntwoord_VoegOpdrachtAntwoordToe_CorrectToegevoegd() {
-		assertFalse("Kopie van OpdrachtAntwoord is leeg" , (opdrachtAntwoord.isEmpty()));
-		OpdrachtAntwoord opdrachtAntwoord1 = opdrachtAntwoord.get(0);
-		assertTrue("Opdrachtantwoord correct toegevoegd", opdrachtAntwoord.add(opdrachtAntwoord1));
-		
+	public void testAddOpdrachtAntwoord_VoegOpdrachtAntwoordToe_CorrectToegevoegd() {
+		Leerling leerling = new Leerling("Lolli", "Pop", 1);
+		quiz1.setQuizStatus(new Opengesteld());
+		QuizDeelname quizDeelname = QuizDeelname.koppelQuizAanLeerling(quiz1, leerling);
+		quizOpdracht = QuizOpdracht.koppelOpdrachtAanQuiz(quiz1, opdracht1, 0);
+		OpdrachtAntwoord opdrachtAntwoord = OpdrachtAntwoord.koppelQuizDeelnameAanQuizOpdracht(quizDeelname, quizOpdracht, 10, 10, "HUDUUH");
+		quizOpdracht.addOpdrachtAntwoord(opdrachtAntwoord);
+		assertTrue("", quizOpdracht.getOpdrachtAntwoorden().contains(opdrachtAntwoord));
 	}
 	
 	@Test
-	public void GetGemiddeldeScore_VerkrijgGemiddeldeScore_CorrecteGemiddeldeScore() {
-		double somScores = 0;
-		for (OpdrachtAntwoord opdrachtAntwoord1 : opdrachtAntwoord) {
+	public void testGetGemiddeldeScore_VerkrijgGemiddeldeScore_CorrecteGemiddeldeScore() {
+		Leerling leerling = new Leerling("Lolli", "Pop", 1);
+		Leerling leerling1 = new Leerling("Jan", "Jansen", 2);
+		quiz1.setQuizStatus(new Opengesteld());
+		quiz2.setQuizStatus(new Opengesteld());
+		QuizDeelname quizDeelname = QuizDeelname.koppelQuizAanLeerling(quiz1, leerling);
+		QuizDeelname quizDeelname1 = QuizDeelname.koppelQuizAanLeerling(quiz2, leerling1);
+		quizOpdracht = QuizOpdracht.koppelOpdrachtAanQuiz(quiz1, opdracht1, 0);
+		QuizOpdracht quizOpdracht1 = QuizOpdracht.koppelOpdrachtAanQuiz(quiz2, opdracht2, 5);
+		OpdrachtAntwoord opdrachtAntwoord2 = OpdrachtAntwoord.koppelQuizDeelnameAanQuizOpdracht(quizDeelname, quizOpdracht, 5, 5, "Audi");
+		OpdrachtAntwoord opdrachtAntwoord3 = OpdrachtAntwoord.koppelQuizDeelnameAanQuizOpdracht(quizDeelname1, quizOpdracht1, 6, 6, "Mercedes");
+		ArrayList<OpdrachtAntwoord> opdrachtAntwoorden;
+		opdrachtAntwoorden = new ArrayList<OpdrachtAntwoord>();
+		opdrachtAntwoorden.add(maxScore, opdrachtAntwoord2);
+		opdrachtAntwoorden.add(maxScore, opdrachtAntwoord3);
+		double somScores = 20;
+		for ( OpdrachtAntwoord opdrachtAntwoord1 : opdrachtAntwoorden) {
 			somScores += opdrachtAntwoord1.getBehaaldeScore();
 		}
-		assertEquals("Correcte Gemiddelde Score", (somScores / opdrachtAntwoord.size()), quizOpdracht.getGemiddeldeScore());
+		assertEquals(10 , (somScores / opdrachtAntwoorden.size()), quizOpdracht.getGemiddeldeScore());
+	}
+	
+	@Test
+	public void testCompareTo_ZelfdeQuizEnOpdracht_Geeft0() {
+		assertEquals(0, opdracht1.compareTo(quizOpdracht.getOpdracht()));
+		assertEquals(0, quiz1.compareTo(quizOpdracht.getQuiz()));
+	}
+	
+	@Test
+	public void testEquals_GeenQuizOpdracht_GeeftFalse() {
+		assertFalse(quizOpdracht.equals(null));
+		assertFalse(quizOpdracht.equals(10));
+		assertFalse(quizOpdracht.equals(50));
+		assertTrue(quizOpdracht.equals(quizOpdracht));
+//		if (this.opdracht1.equals(other.getOpdracht())){
+//			assertFalse(quizOpdracht.equals(1));
+//		}
+	}
+	
+	@Test
+	public void testToString_GeenParam_IsOk() {
+		String result = "QuizOpdracht [ID=" + quizOpdracht.getID() + "] - Maximale score: " + maxScore;
+		result += "\nDIE\n" + opdracht1.toString().replaceAll("(?m)^", "\t");;
+		result += "\nKOPPELT AAN\n" + quiz1.toString().replaceAll("(?m)^", "\t");;
+		assertEquals(result , quizOpdracht.toString());
 	}
 }
