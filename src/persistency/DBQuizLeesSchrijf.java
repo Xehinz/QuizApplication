fd22c88ee7186f4d1800a50e8502f367311a8477
jefke222 <jefke222@hotmail.com>
2014-12-12 23:33:15 +0100

    DBQuizLeesSchrijf I
    IsTest() en DoelLeerjaren toegevoegd.

--- src/persistency/DBQuizLeesSchrijf.java
+++ src/persistency/DBQuizLeesSchrijf.java
@@ -3,9 +3,18 @@ package persistency;
 import java.io.IOException;
 
 import util.datumWrapper.Datum;
+
 import model.Leraar;
 import model.Quiz;
 import model.quizStatus.QuizStatus;
+
+/**
+ * Klasse om Quiz objecten weg te schrijven of in te lezen in een Database
+ *
+ * @author Jef Bellens
+ * @version 12/12/2014
+ *
+ */
 
 public class DBQuizLeesSchrijf extends DBTemplate {
 
@@ -22,13 +31,14 @@ public class DBQuizLeesSchrijf extends DBTemplate {
 	@Override
 	protected Quiz maakObject(Object[] rij) {
 		int ID = (Integer) rij[0];
-		Datum aanmaakDatum = (Datum) rij [1];
-		QuizStatus status = (QuizStatus) rij [2];
-		Leraar auteurNaam = (Leraar) rij [3];
-		String onderwerp = (String) rij [4];
-		Boolean isuniekeDeelname = (Boolean) rij [5];
-//		Boolean isTest = (Boolean) rij [6];
-		return new Quiz (ID, aanmaakDatum, status, auteurNaam, onderwerp, isuniekeDeelname);
+		Datum aanmaakDatum = (Datum) rij[1];
+		QuizStatus status = (QuizStatus) rij[2];
+		Leraar auteurNaam = (Leraar) rij[3];
+		String onderwerp = (String) rij[4];
+		Boolean isuniekeDeelname = (Boolean) rij[5];
+		// Boolean isTest = (Boolean) rij [6];
+		return new Quiz(ID, aanmaakDatum, status, auteurNaam, onderwerp,
+				isuniekeDeelname);
 	}
 
 	@Override
@@ -37,17 +47,24 @@ public class DBQuizLeesSchrijf extends DBTemplate {
 		if (object instanceof Quiz) {
 			quiz = (Quiz) object;
 		} else {
-			throw new IOException(
-					"Het object om weg te schrijven is geen quiz");
+			throw new IOException("Het object om weg te schrijven is geen quiz");
 		}
+		
+		String doelLeerjaren = "";
+		for (int i = 0; i < quiz.getDoelLeerjaren().size(); i++) {
+			if(i != quiz.getDoelLeerjaren().size())
+				doelLeerjaren += quiz.getDoelLeerjaren().get(i) + ",";
+			else
+				doelLeerjaren += quiz.getDoelLeerjaren().get(i).toString();
+		}
+				
+		
 		return String.format(
-		"INSERT INTO quiz VALUES('%d', '%tF', %s, %s, %s, %s)",
-		quiz.getID(),
-		quiz.getAanmaakDatum().getDatumInEuropeesFormaat(),
-		quiz.getQuizStatus().toString(),
-		quiz.getAuteur(),
-		quiz.getOnderwerp(),
-		quiz.getIsUniekeDeelname());
+				"INSERT INTO quiz VALUES('%d', '%tF', %s, %s, %s, %s)", quiz
+						.getID(), quiz.getAanmaakDatum()
+						.getDatumInEuropeesFormaat(), quiz.getQuizStatus()
+						.toString(), quiz.getAuteur().name(), quiz.getOnderwerp(),
+				quiz.getIsUniekeDeelname(), quiz.getIsTest(), doelLeerjaren);
 	}
 
 	@Override
